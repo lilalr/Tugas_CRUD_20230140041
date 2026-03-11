4d59789f4524b56eb3df8b8b6892a5540c595c37
lilalr <gurennut@gmail.com>
2026-03-11 17:05:20 +0700

    membuat dto ktp dan dto ktpaddrequest

---
+++ src/main/java/com/example/ktp_crud/model/dto/KtpAddRequest.java
@@ -0,0 +1,13 @@
+package com.example.ktp_crud.model.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class KtpAddRequest {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
