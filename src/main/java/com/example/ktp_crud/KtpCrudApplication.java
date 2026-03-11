ceeb44af0694b7428a4b1c1b7a6d225cabbe1fbc
lilalr <gurennut@gmail.com>
2026-03-11 16:59:26 +0700

    Initial commit

---
+++ src/main/java/com/example/ktp_crud/KtpCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktp_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktp_crud/KtpCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktp_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
