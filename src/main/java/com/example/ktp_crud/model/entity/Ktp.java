7100186cd50528818bc9c2b7a2aa5607d8bc6032
lilalr <gurennut@gmail.com>
2026-03-11 17:04:58 +0700

    membuat entity ktp

---
+++ src/main/java/com/example/ktp_crud/model/entity/Ktp.java
@@ -0,0 +1,36 @@
+package com.example.ktp_crud.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ktp")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nomor_ktp", unique = true, nullable = false)
+    private String nomorKtp;
+
+    @Column(name = "nama_lengkap", nullable = false)
+    private String namaLengkap;
+
+    @Column(name = "alamat")
+    private String alamat;
+
+    @Column(name = "tanggal_lahir")
+    private LocalDate tanggalLahir;
+
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+}
